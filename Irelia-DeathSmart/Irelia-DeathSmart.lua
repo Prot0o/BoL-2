59f415d4d746ff44784c2093f206cb6d8737482d
desperadisse <despe.ogame@gmail.com>
2016-03-31 23:14:07 +0200

    Update Auto-Update by Spyk

--- Irelia-DeathSmart/Irelia-DeathSmart.lua
+++ Irelia-DeathSmart/Irelia-DeathSmart.lua
@@ -22,16 +22,16 @@ if AUTOUPDATE then
 		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
 		if ServerVersion then
 			if tonumber(version) < ServerVersion then
-				EnvoiMessage("New version available "..ServerVersion)
-				EnvoiMessage(">>Updating, please don't press F9<<")
-				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () EnvoiMessage("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
+				print("New version available "..ServerVersion)
+				print(">>Updating, please don't press F9<<")
+				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () print("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
 				whatsnew = 1
 			else
-				DelayAction(function() EnvoiMessage("Hello, "..GetUser()..". You got the latest version! :) ("..ServerVersion..")") end, 3)
+				DelayAction(function() print("Hello, "..GetUser()..". You got the latest version! :) ("..ServerVersion..")") end, 3)
 			end
 		end
 		else
-			EnvoiMessage("Error downloading version info")
+			print("Error downloading version info")
 	end
 end
  --- End Of AutoUpdate
